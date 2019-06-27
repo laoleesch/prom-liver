3889c4fc6ffb6dcaba53777fbe2becfc0e9963b1
Alexander.Lishchuk <alexander.lishchuk@x5.ru>
2019-06-27 13:15:51 +0300

    add clients yaml

--- main.go
+++ main.go
@@ -16,28 +16,42 @@ import (
 
 type AppConfig struct {
 	ConfigFile string
-	Server     ServerConfig  `yaml:"server,omitempty"`
-	Clients    ClientsConfig `yaml:",omitempty"` // TODO!
+	Server     ServerConfig   `yaml:"server,omitempty"`
+	Clients    []ClientConfig `yaml:"clients"`
 }
 
 type ServerConfig struct {
-	Port  string
-	Proxy string
-	Auth  []string
+	Port        string   `yaml:"port,omitempty"`
+	Proxy       string   `yaml:"proxy,omitempty"`
+	Authschemes []string `yaml:"authschemes,omitempty"`
 }
 
-type ClientsConfig struct {
+type ClientConfig struct {
+	Id    string     `yaml:"id"`
+	Auth  AuthSchema `yaml:"auth"`
+	Match []string   `yaml:"match"`
+}
+
+type AuthSchema struct {
+	IP    string          `yaml:"ip,omitempty"`
+	Basic AuthSchemaBasic `yaml:"basic,omitempty"`
+}
+
+type AuthSchemaBasic struct {
+	User     string `yaml:"user,omitempty"`
+	Password string `yaml:"password,omitempty"`
 }
 
 var (
 	err error
 
+	// default config
 	cfg = AppConfig{
 		ConfigFile: "config.yaml",
 		Server: ServerConfig{
 			Port:  "8080",
 			Proxy: "http://localhost:9090",
-			Auth: []string{
+			Authschemes: []string{
 				"ip",
 			},
 		},
@@ -52,8 +66,7 @@ func main() {
 	c.HelpFlag.Short('h')
 
 	// get configfile
-	c.Flag("configfile", "Configuration file path.").
-		Default("config.yaml").StringVar(&cfg.ConfigFile)
+	c.Flag("configfile", "Configuration file path.").StringVar(&cfg.ConfigFile)
 
 	// read configfile
 	file, err := ioutil.ReadFile(cfg.ConfigFile)
