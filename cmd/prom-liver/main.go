4d3b157f2e5c3cf1032e38ebae29d6656e61cda2
laoleesch <laoleesch@MacBook-Pro.local>
2020-10-06 19:37:57 +0300

    chng config structure

--- cmd/prom-liver/main.go
+++ cmd/prom-liver/main.go
@@ -5,6 +5,7 @@ import (
 	// _ "net/http/pprof"
 
 	"context"
+	"encoding/base64"
 	"fmt"
 	stdlog "log"
 	"net/http"
@@ -12,7 +13,6 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"strings"
 	"syscall"
 	"time"
@@ -34,45 +34,39 @@ var (
 	logger kitlog.Logger
 
 	// cmd args
-	cmdConfigFile string
-	cmdLogLevel   string
+	configFile    = kingpin.Flag("config", "Configuration file").Short('c').Default("config.yaml").String()
+	logLevel      = kingpin.Flag("loglevel", "Log filtering level").Short('l').Default("info").Enum("debug", "info", "warning", "error")
+	listenAddress = kingpin.Flag("bind", "Address to listen on.").Short('b').Default(":8080").String()
 
-	// global config
+	// Cfg global config
 	Cfg config.Config
 
 	// reload config channel
 	chHTTPReload chan chan error
 
 	// managers
-	cmp *config.ConfigManager
+	cmp *config.Manager
 	amp *auth.Manager
 	fmp *filter.Manager
 )
 
 func main() {
 
-	c := kingpin.New(filepath.Base(os.Args[0]), "ACL for PromQL")
-	c.HelpFlag.Short('h')
-	c.Flag("loglevel", "Log level: debug, info, warning, error").Default("info").Short('l').StringVar(&cmdLogLevel)
-	c.Flag("config", "Configuration file").Short('c').Default("config.yaml").StringVar(&cmdConfigFile)
-	_, err := c.Parse(os.Args[1:])
-	if err != nil {
-		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing commandline arguments"))
-		c.Usage(os.Args[1:])
-		os.Exit(2)
-	}
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
 
 	// init logger
-	logger = initLogger(cmdLogLevel)
-	level.Info(logger).Log("loglevel", cmdLogLevel)
-	level.Info(logger).Log("configfile", cmdConfigFile)
+	logger = initLogger(*logLevel)
+	level.Info(logger).Log("loglevel", *logLevel)
+	level.Info(logger).Log("configfile", *configFile)
+	level.Info(logger).Log("bind", *listenAddress)
 
 	// Init config
-	Cfg = config.DefaultConfig
+	Cfg = config.DefaultConfig()
 	amp = auth.NewManager(&logger)
 	fmp = filter.NewManager(&logger)
 
-	cmp, err = config.New(cmdConfigFile, &logger)
+	cmp, err := config.New(*configFile, &logger)
 	if err != nil {
 		level.Error(logger).Log("msg", "Error init ConfigManager", "err", err)
 		os.Exit(2)
@@ -84,10 +78,9 @@ func main() {
 		os.Exit(2)
 	}
 
-	level.Info(logger).Log("server.port", Cfg.Server.Port)
-	level.Info(logger).Log("server.proxy", Cfg.Server.Proxy)
-	level.Info(logger).Log("server.authentication", Cfg.Server.Authentication)
-	level.Info(logger).Log("server.id-header", Cfg.Server.HeaderName)
+	level.Info(logger).Log("web.auth", Cfg.Web.Auth)
+	level.Info(logger).Log("web.header", Cfg.Web.HeaderName)
+	level.Info(logger).Log("remote.url", Cfg.Remote.URL)
 
 	// config reload handler
 	go func() {
@@ -102,7 +95,7 @@ func main() {
 				if err := reloadConfig(cmp); err != nil {
 					level.Error(logger).Log("msg", "Error reloading config", "err", err)
 				} else {
-					level.Info(logger).Log("msg", "Config has been successfully reloaded", "file", cmdConfigFile)
+					level.Info(logger).Log("msg", "Config has been successfully reloaded", "file", *configFile)
 				}
 			case rc := <-chHTTPReload:
 				level.Info(logger).Log("msg", "got http config reload signal")
@@ -110,7 +103,7 @@ func main() {
 					level.Error(logger).Log("msg", "Error reloading config", "err", err)
 					rc <- err
 				} else {
-					level.Info(logger).Log("msg", "Config has been successfully reloaded", "file", cmdConfigFile)
+					level.Info(logger).Log("msg", "Config has been successfully reloaded", "file", *configFile)
 					rc <- nil
 				}
 			}
@@ -120,31 +113,36 @@ func main() {
 	// run handlers
 	r := mux.NewRouter()
 	r = r.Methods("GET").Subrouter()
-	if Cfg.Server.Authentication {
+	if Cfg.Web.Auth {
 		r.Use(amp.CheckAuth)
 	}
 
-	if Cfg.Server.API {
-		r.Handle("/api/v1/series", fmp.FilterQuery("match[]", serveReverseProxy(Cfg.Server.Proxy))).Methods("GET")
+	if Cfg.Web.Handlers.API {
+		r.Handle("/api/v1/series", fmp.FilterQuery("match[]", serveReverseProxy(Cfg.Remote.URL))).Methods("GET")
 		level.Info(logger).Log("server.uri", "/api/v1/series", "server.uri.methods", "GET")
-		r.Handle("/api/v1/query", fmp.FilterQuery("query", serveReverseProxy(Cfg.Server.Proxy))).Methods("GET")
+		r.Handle("/api/v1/query", fmp.FilterQuery("query", serveReverseProxy(Cfg.Remote.URL))).Methods("GET")
 		level.Info(logger).Log("server.uri", "/api/v1/query", "server.uri.methods", "GET")
-		r.Handle("/api/v1/query_range", fmp.FilterQuery("query", serveReverseProxy(Cfg.Server.Proxy))).Methods("GET")
+		r.Handle("/api/v1/query_range", fmp.FilterQuery("query", serveReverseProxy(Cfg.Remote.URL))).Methods("GET")
 		level.Info(logger).Log("server.uri", "/api/v1/query_range", "server.uri.methods", "GET")
 	}
-	if Cfg.Server.Federate {
-		r.Handle("/federate", fmp.FilterQuery("match[]", serveReverseProxy(Cfg.Server.Proxy))).Methods("GET")
+	if Cfg.Web.Handlers.Federate {
+		r.Handle("/federate", fmp.FilterQuery("match[]", serveReverseProxy(Cfg.Remote.URL))).Methods("GET")
 	}
-	if Cfg.Server.APIVMLabels {
-		r.Handle("/api/v1/label/{label}/values", fmp.FilterQuery("match[]", serveReverseProxy(Cfg.Server.Proxy))).Methods("GET")
+	if Cfg.Web.Handlers.APIVMLabels {
+		r.Handle("/api/v1/label/{label}/values", fmp.FilterQuery("match[]", serveReverseProxy(Cfg.Remote.URL))).Methods("GET")
 		level.Info(logger).Log("server.uri", "/api/v1/label/*/valuese", "server.uri.methods", "GET")
-		r.Handle("/api/v1/labels", fmp.FilterQuery("match[]", serveReverseProxy(Cfg.Server.Proxy))).Methods("GET")
+		r.Handle("/api/v1/labels", fmp.FilterQuery("match[]", serveReverseProxy(Cfg.Remote.URL))).Methods("GET")
 		level.Info(logger).Log("server.uri", "/api/v1/labels", "server.uri.methods", "GET")
+	}
+
+	if Cfg.Web.Handlers.ConfigReload {
+		r.Handle("/-/config/reload", reloadConfigHandler()).Methods("POST", "PUT")
+		level.Info(logger).Log("server.uri", "/-/config/reload", "server.uri.methods", "POST,PUT")
 	}
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         ":" + Cfg.Server.Port,
+		Addr:         *listenAddress,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
@@ -155,24 +153,6 @@ func main() {
 			os.Exit(2)
 		}
 	}()
-
-	if Cfg.Server.AdminAPI {
-		ra := mux.NewRouter()
-		ra.Handle("/admin/config/reload", reloadConfigHandler()).Methods("POST", "PUT")
-		level.Info(logger).Log("admin.port", Cfg.Server.AdminPort)
-		level.Info(logger).Log("admin.uri", "/admin/config/reload", "admin.uri.methods", "POST,PUT")
-		srvadmin := &http.Server{
-			Handler:      ra,
-			Addr:         ":" + Cfg.Server.AdminPort,
-			WriteTimeout: 15 * time.Second,
-			ReadTimeout:  15 * time.Second,
-		}
-		go func() {
-			if err := srvadmin.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-				level.Error(logger).Log("msg", "listener admin http error", "err", err)
-			}
-		}()
-	}
 
 	chStop := make(chan os.Signal, 1)
 	signal.Notify(chStop, os.Interrupt, syscall.SIGTERM)
@@ -203,25 +183,25 @@ func initLogger(s string) kitlog.Logger {
 	case "error":
 		logger = level.NewFilter(logger, level.AllowError())
 	default:
-		level.Error(logger).Log("msg", "wrong log level name", "value", cmdLogLevel)
+		level.Error(logger).Log("msg", "wrong log level name", "value", *logLevel)
 		logger = level.NewFilter(logger, level.AllowInfo())
 	}
 	return logger
 }
 
-func reloadConfig(cmp *config.ConfigManager) error {
+func reloadConfig(cmp *config.Manager) error {
 	cfg, err := cmp.LoadConfig()
 	if err != nil {
 		return errors.Wrapf(err, "cannot load config file ")
 	}
 
 	newAmp := auth.NewManager(&logger)
-	if cfg.Server.Authentication {
+	if cfg.Web.Auth {
 		authMemMap, err := config.ExtractAuthMap(&cfg)
 		if err != nil {
 			return errors.Wrapf(err, "error extracting auth map from config")
 		}
-		err = newAmp.ApplyConfig(cfg.Server.HeaderName, authMemMap)
+		err = newAmp.ApplyConfig(cfg.Web.HeaderName, authMemMap)
 		if err != nil {
 			return errors.Wrapf(err, "error create new auth config")
 		}
@@ -232,7 +212,7 @@ func reloadConfig(cmp *config.ConfigManager) error {
 	if err != nil {
 		return errors.Wrapf(err, "error extracting filter map from config")
 	}
-	err = newFmp.ApplyConfig(cfg.Server.HeaderName, matchMap, injectMap, filterMap)
+	err = newFmp.ApplyConfig(cfg.Web.HeaderName, matchMap, injectMap, filterMap)
 	if err != nil {
 		return errors.Wrapf(err, "error create new filter config")
 	}
@@ -257,7 +237,7 @@ func reloadConfig(cmp *config.ConfigManager) error {
 func serveReverseProxy(target string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		if Cfg.Server.RemoteInsecureSkipVerify {
+		if !Cfg.Remote.TLS.Verify {
 			http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 		}
 
@@ -266,8 +246,12 @@ func serveReverseProxy(target string) http.Handler {
 		r.URL.Host = url.Host
 		r.URL.Scheme = url.Scheme
 		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
-		if Cfg.Server.RemoteAuth != "" {
-			r.Header.Set("Authorization", Cfg.Server.RemoteAuth)
+		if Cfg.Remote.Auth.User != "" && Cfg.Remote.Auth.Password != "" {
+			r.Header.Set("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(Cfg.Remote.Auth.User))+
+				":"+base64.StdEncoding.EncodeToString([]byte(Cfg.Remote.Auth.Password)))
+		}
+		if Cfg.Remote.Auth.Token != "" {
+			r.Header.Set("Authorization", "Bearer "+Cfg.Remote.Auth.Token)
 		}
 		r.Host = url.Host
 		r.RequestURI = url.EscapedPath() + r.RequestURI
--- internal/config/config.go
+++ internal/config/config.go
@@ -11,53 +11,58 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-var (
-	// DefaultConfig is the default top-level configuration.
-	DefaultConfig = Config{
-		Server: DefaultServerConfig,
-	}
-
-	// DefaultServerConfig is the default global configuration.
-	DefaultServerConfig = ServerConfig{
-
-		API:            true,
-		APIVMLabels:    false,
-		Federate:       true,
-		Proxy:          "http://localhost:9090/",
-		Authentication: true,
-		HeaderName:     "X-Prom-Liver-Id",
-		AdminAPI:       true,
-		AdminPort:      "8888",
-
-		RemoteAuth:               "",
-		RemoteInsecureSkipVerify: false,
-	}
-)
-
 // Config includes all config.yaml
 type Config struct {
-	Server       ServerConfig `yaml:"server,omitempty"`
-	ClientsFiles []string     `yaml:"clients_files,omitempty"`
-	Clients      Clients      `yaml:"clients,omitempty"`
+	Web    WebConfig    `yaml:"web,omitempty"`
+	Remote RemoteConfig `yaml:"remote,omitempty"`
+
+	ClientsFiles []string `yaml:"clients_files,omitempty"`
+	Clients      Clients  `yaml:"clients,omitempty"`
 }
 
-// ServerConfig includes only "server:" three
-type ServerConfig struct {
-	API            bool `yaml:"api-enable,omitempty"`
-	APIVMLabels    bool `yaml:"api-labels-enable,omitempty"` // extended promql handlers for VictoriaMetrics
-	Federate       bool `yaml:"federate-enable,omitempty"`
-	Authentication bool `yaml:"authentication,omitempty"`
+// WebConfig config
+type WebConfig struct {
+	Auth       bool   `yaml:"auth,omitempty"`
+	HeaderName string `yaml:"header,omitempty"`
+	Handlers   WebHandlersConfig
+}
 
-	AdminAPI bool `yaml:"admin-api-enable,omitempty"`
+// WebHandlersConfig handlers configs
+type WebHandlersConfig struct {
+	API          bool `yaml:"api,omitempty"`
+	Federate     bool `yaml:"federate,omitempty"`
+	APIVMLabels  bool `yaml:"labels,omitempty"`
+	ConfigReload bool `yaml:"config_reload,omitempty"`
+}
 
-	RemoteInsecureSkipVerify bool `yaml:"remote-insecure-skip-verify,omitempty"`
+// RemoteConfig includes configs for remote PromQL service
+type RemoteConfig struct {
+	URL  string           `yaml:"url,omitempty"`
+	Auth RemoteAuthConfig `yaml:"auth,omitempty"`
+	TLS  RemoteTLSConfig  `yaml:"tls,omitempty"`
+}
 
-	AdminPort  string `yaml:"admin-port,omitempty"`
-	Port       string `yaml:"port,omitempty"`
-	Proxy      string `yaml:"proxy,omitempty"`
-	HeaderName string `yaml:"id-header,omitempty"`
+// RemoteAuthConfig includes auth configs for remote PromQL service
+type RemoteAuthConfig struct {
+	User     string `yaml:"user,omitempty"`
+	Password string `yaml:"password,omitempty"`
+	Token    string `yaml:"token,omitempty"`
+}
 
-	RemoteAuth string `yaml:"remote-auth,omitempty"`
+func (c *RemoteAuthConfig) String() string {
+	result := ""
+	if c.Password != "" || c.User != "" {
+		result = fmt.Sprintf("%s basic: true \n", result)
+	}
+	if c.Token != "" {
+		result = fmt.Sprintf("%s bearer: true \n", result)
+	}
+	return result
+}
+
+// RemoteTLSConfig includes TLS configs for remote PromQL service
+type RemoteTLSConfig struct {
+	Verify bool `yaml:"verify,omitempty"`
 }
 
 //Clients includes configuration for each client
@@ -96,28 +101,73 @@ type AuthSchemaBearer struct {
 	Files  []string `yaml:"files,omitempty"`
 }
 
-type ConfigManager struct {
+func (c *AuthSchema) String() string {
+	result := ""
+
+	if c.Header {
+		result = fmt.Sprintf("%s header: true \n", result)
+	}
+	if c.Basic.User != "" && c.Basic.Password != "" {
+		result = fmt.Sprintf("%s auth basic user password: true \n", result)
+	}
+	if len(c.Basic.Base64) > 0 {
+		result = fmt.Sprintf("%s auth basic base64: %d \n", result, len(c.Basic.Base64))
+	}
+	if len(c.Bearer.Tokens) > 0 {
+		result = fmt.Sprintf("%s auth bearer tokens: %d \n", result, len(c.Bearer.Tokens))
+	}
+	return result
+}
+
+// Manager controls config from file
+type Manager struct {
 	configFile string
 	logger     kitlog.Logger
 }
 
-// New creates ConfigManager
-func New(cf string, l *kitlog.Logger) (*ConfigManager, error) {
+// New creates Manager
+func New(cf string, l *kitlog.Logger) (*Manager, error) {
 	if l == nil {
 		return nil, fmt.Errorf("logger is nil")
 	}
 	if cf == "" {
 		return nil, fmt.Errorf("filename is empty")
 	}
-	cm := &ConfigManager{
+	cm := &Manager{
 		configFile: cf,
 		logger:     *l,
 	}
 	return cm, nil
 }
 
+// DefaultConfig returns default configuration
+func DefaultConfig() Config {
+	return Config{
+		Web: WebConfig{
+			Auth:       true,
+			HeaderName: "X-Prom-Liver-Id",
+			Handlers: WebHandlersConfig{
+				API:          true,
+				Federate:     true,
+				APIVMLabels:  false,
+				ConfigReload: true,
+			},
+		},
+		Remote: RemoteConfig{
+			Auth: RemoteAuthConfig{
+				User:     "",
+				Password: "",
+				Token:    "",
+			},
+			TLS: RemoteTLSConfig{
+				Verify: false,
+			},
+		},
+	}
+}
+
 // LoadConfig returns Config after it reads and parse all config files
-func (cm *ConfigManager) LoadConfig() (newCfg Config, err error) {
+func (cm *Manager) LoadConfig() (newCfg Config, err error) {
 	newCfg, err = cm.loadConfigFile()
 	if err != nil {
 		return newCfg, errors.Wrapf(err, "cannot load config file ")
@@ -142,8 +192,8 @@ func (cm *ConfigManager) LoadConfig() (newCfg Config, err error) {
 	return
 }
 
-func (cm *ConfigManager) loadConfigFile() (newCfg Config, err error) {
-	newCfg = DefaultConfig
+func (cm *Manager) loadConfigFile() (newCfg Config, err error) {
+	newCfg = DefaultConfig()
 	level.Debug(cm.logger).Log("msg", "read file", "file", cm.configFile)
 	file, err := ioutil.ReadFile(cm.configFile)
 	if err != nil {
@@ -168,7 +218,7 @@ func findFiles(patFiles []string) (files []string, err error) {
 	return
 }
 
-func (cm *ConfigManager) loadClientsConfigFiles(newCfg Config) (Config, error) {
+func (cm *Manager) loadClientsConfigFiles(newCfg Config) (Config, error) {
 	level.Debug(cm.logger).Log("msg", "try to find clients config files", "patterns", fmt.Sprint(newCfg.ClientsFiles))
 	files, err := findFiles(newCfg.ClientsFiles)
 	if err != nil {
@@ -201,7 +251,7 @@ func (cm *ConfigManager) loadClientsConfigFiles(newCfg Config) (Config, error) {
 	return newCfg, nil
 }
 
-func (cm *ConfigManager) loadCredsFiles(newCfg Config) (Config, error) {
+func (cm *Manager) loadCredsFiles(newCfg Config) (Config, error) {
 	for id, clientConfig := range newCfg.Clients {
 		// read base64 files and copy to []Base64
 		if len(clientConfig.Auth.Basic.Files) > 0 {
@@ -226,7 +276,7 @@ func (cm *ConfigManager) loadCredsFiles(newCfg Config) (Config, error) {
 	return newCfg, nil
 }
 
-func (cm *ConfigManager) readCredsFiles(patFiles []string) ([]string, error) {
+func (cm *Manager) readCredsFiles(patFiles []string) ([]string, error) {
 	filesContent := make([]string, 0)
 	files, err := findFiles(patFiles)
 	if err != nil {
@@ -243,30 +293,4 @@ func (cm *ConfigManager) readCredsFiles(patFiles []string) ([]string, error) {
 	}
 
 	return filesContent, nil
-}
-
-func (c *ServerConfig) String() string {
-	b, err := yaml.Marshal(c)
-	if err != nil {
-		return fmt.Sprintf("<error creating config string: %s>", err)
-	}
-	return string(b)
-}
-
-func (c *AuthSchema) String() string {
-	result := ""
-
-	if c.Header {
-		result = fmt.Sprintf("%s header: true \n", result)
-	}
-	if c.Basic.User != "" && c.Basic.Password != "" {
-		result = fmt.Sprintf("%s auth basic user password: true \n", result)
-	}
-	if len(c.Basic.Base64) > 0 {
-		result = fmt.Sprintf("%s auth basic base64: %d \n", result, len(c.Basic.Base64))
-	}
-	if len(c.Bearer.Tokens) > 0 {
-		result = fmt.Sprintf("%s auth bearer tokens: %d \n", result, len(c.Bearer.Tokens))
-	}
-	return result
 }
